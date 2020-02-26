7f9cdedb5b4b0f49d56dd209bfc5c4c14b66e9fc
Jordan <whousesversioncontrol@gmail.com>
2020-02-25 19:00:00 -0800

    ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'consistent-return': 'off',
+    'default-case': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'no-plusplus': ['warn', { 'allowForLoopAfterthoughts': true }],
+    'no-underscore-dangle': ['warn', { 'allowAfterThis': true }],
+    'react/jsx-filename-extension': ['warn', { 'extensions': ['.jsx', '.tsx'] }],
+  },
+  overrides: [
+    {
+      'files': ['webpack.config.js'],
+      'rules': {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+};
