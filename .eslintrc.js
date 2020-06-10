e0c918a77b1ea2fc4bc6821a699fbf93aaec0e0a
Jordan <whousesversioncontrol@gmail.com>
2020-06-10 12:52:34 -0700

    Update dependencies and ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,34 @@
 module.exports = {
   env: {
     browser: true,
-    jest: true
+    jest: true,
   },
   extends: [
     'airbnb',
     'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
-    "plugin:jest/all",
+    'plugin:jest/all',
   ],
   parser: '@typescript-eslint/parser',
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'consistent-return': 'off',
     'default-case': 'off',
     'import/extensions': 'off',
     'import/no-unresolved': 'off',
-    'jest/lowercase-name': ['error', { 'ignore': ['describe'] }],
-    'jest/no-hooks': ['error', { 'allow': ['beforeEach'] }],
-    'no-plusplus': ['warn', { 'allowForLoopAfterthoughts': true }],
-    'no-underscore-dangle': ['warn', { 'allowAfterThis': true }],
-    'react/jsx-filename-extension': ['warn', { 'extensions': ['.jsx', '.tsx'] }],
+    'jest/lowercase-name': ['error', { ignore: ['describe'] }],
+    'jest/no-hooks': ['error', { allow: ['beforeEach'] }],
+    'no-plusplus': ['warn', { allowForLoopAfterthoughts: true }],
+    'no-underscore-dangle': ['warn', { allowAfterThis: true }],
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.tsx'] }],
     'react/no-array-index-key': 'off',
     'react/prop-types': 'off',
   },
   overrides: [
     {
-      'files': ['webpack.config.js'],
-      'rules': {
+      files: ['webpack.config.js'],
+      rules: {
         '@typescript-eslint/no-var-requires': 'off',
       },
     },
